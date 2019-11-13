7179cb95f99b12d63bc2f50687464a7eab97dbd8
treemmett <tregan@tregan.me>
2019-11-12 23:04:02 -0700

    feat: add lint rules

---
+++ README.md
@@ -0,0 +1,21 @@
+# eslint-config-zyehex
+
+## Installation
+
+```bash
+# with yarn
+yarn add -D eslint eslint-config-zyehex
+
+# or if you prefer npm
+npm i -D eslint eslint-config-zyehex
+```
+
+## Usage
+
+Once the `eslint-config-zyehex` package is installed, you can use it by [extending](http://eslint.org/docs/user-guide/configuring#extending-configuration-files) it in your [ESLint configuration file](http://eslint.org/docs/user-guide/configuring).
+
+```json
+{
+  "extends": "zyehex"
+}
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    '@typescript-eslint/explicit-member-accessibility': 2,
+    '@typescript-eslint/member-ordering': 2,
+    '@typescript-eslint/no-unused-vars': 2,
+    'import/no-cycle': 0,
+    'import/order': 0,
+    'no-alert': 2,
+    'no-console': 2,
+    'prettier/prettier': [2, { singleQuote: true }],
+    'sort-imports': 2
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  }
+};
