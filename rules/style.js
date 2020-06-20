4db2c9f6059d3b51260ed0e05b7b3ac1e8dc4943
treemmett <tregan@tregan.me>
2020-06-19 23:59:05 -0600

    fix: new prettier defaults

--- rules/style.js
+++ rules/style.js
@@ -75,10 +75,21 @@ module.exports = {
     'prettier/prettier': [
       'error',
       {
-        arrowParens: 'avoid',
+        arrowParens: 'always',
+        bracketSpacing: true,
+        endOfLine: 'lf',
+        htmlWhitespaceSensitivity: 'css',
+        jsxBracketSameLine: false,
+        jsxSingleQuote: false,
+        printWidth: 80,
+        proseWrap: 'preserve',
+        quoteProps: 'as-needed',
+        semi: true,
         singleQuote: true,
         tabWidth: 2,
+        trailingComma: 'es5',
         useTabs: false,
+        vueIndentScriptAndStyle: false,
       },
     ],
     'sort-keys': [
