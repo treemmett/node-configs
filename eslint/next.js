dc189052d7e5dec38eafe1947889ffb50bd870ff
Tregan <tregan@tregan.me>
2023-06-19 11:35:05 +0200

    chore: update rules

--- eslint/next.js
+++ eslint/next.js
@@ -2,9 +2,11 @@ module.exports = {
   extends: ['next/core-web-vitals', 'next', './index'],
   overrides: [
     {
-      files: ['next*.js'],
+      files: ['next*.js', 'i18next-parser.config.ts'],
       rules: {
         '@typescript-eslint/no-var-requires': 'off',
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+        'no-await-in-loop': 'off',
       },
     },
   ],
